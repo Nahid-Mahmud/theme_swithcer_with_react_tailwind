8d5058dfc25e65d90a749e387cf5add4a262a8b5
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-03-29 23:00:29 +0600

    initial setup completed (installed tailwind css and configured)

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Fragment>
+  );
+}
+
+export default App;
